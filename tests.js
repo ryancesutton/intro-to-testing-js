06d9e17e518f0df874ef4b8f91d270c0606706d9
Ry Sutton <ryancesutton@gmail.com>
2020-03-23 15:10:55 -0500

    add tests and ability to say 'hello' to any input.

--- tests.js
+++ tests.js
@@ -18,4 +18,16 @@ describe('sayHello', function () {
     it('should be a defined function', function () {
         expect(typeof sayHello).toBe('function');
     });
+    it('should return a string when called', function () {
+        expect(typeof sayHello()).toBe('string');
+    });
+    it('should return the string "Hello, Jane!"', function () {
+        expect(sayHello("Jane")).toBe("Hello, Jane!");
+    });
+    it('should return the string "Hello, Alex!"', function () {
+        expect(sayHello("Alex")).toBe("Hello, Alex!");
+    });
+    it('should return the string "Hello, Pat!"', function () {
+        expect(sayHello("Pat")).toBe("Hello, Pat!");
+    });
 });
